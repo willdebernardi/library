c118d521ecb55c693924636782e25595ff199fb9
Will Debernardi <wdebernardi@unm.edu>
2021-04-25 02:45:26 -0600

    Finish implementing basic library system

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ addNewBookButton.addEventListener("click", function() {
 
 let submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", function() {
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
-    let pages = document.getElementById("pages").value;
-    let read = document.getElementById("read").value;
-    addBookToLibrary(title, author, pages, read);
+    let title = (document.getElementById("title")).value;
+    let author = (document.getElementById("author")).value;
+    let pages = (document.getElementById("pages")).value;
+    let readStatus = (document.getElementById("read")).checked;
+    addBookToLibrary(title, author, pages, readStatus);
+    document.getElementById("form-container").style.display = "none";
+    (document.getElementById("new-book")).reset();
 })
 
 class Book {
@@ -32,8 +34,58 @@ class Book {
 function addBookToLibrary(title, author, pages, readStatus) {
     let book = new Book(title, author, pages, readStatus);
     myLibrary.push(book);
+    updateDisplay();
 }
 
 function updateDisplay() {
-    
+    let book = myLibrary[myLibrary.length - 1];
+    let bookGrid = document.getElementById("book-grid");
+    let cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    let titleH = document.createElement("h2");
+    titleH.classList.add("card-text", "title");
+    let authorH = document.createElement("h2");
+    authorH.classList.add("card-text", "author");
+    let pagesH = document.createElement("h2");
+    pagesH.classList.add("card-text", "pages");
+    let buttonRow = document.createElement("div");
+    buttonRow.classList.add("button-row");
+    let readButton = document.createElement("button");
+    readButton.classList.add("card-button", "read-button");
+    let removeButton = document.createElement("button");
+    removeButton.classList.add("card-button", "remove");
+
+    titleH.textContent = "\""+ book.title + "\"";
+    authorH.textContent = book.author;
+    pagesH.textContent = "Pages: " + book.pages;
+    readButton.textContent = "Read";
+    removeButton.textContent = "Remove";
+
+    if (book.read === true) {
+        readButton.textContent = "Read";
+    } else {
+        readButton.textContent = "Not Read";
+    }
+
+    readButton.addEventListener("click", function() {
+        this.classList.toggle("read-button-active");
+        if(this.textContent === "Read") {
+            this.textContent = "Not Read";
+        } else {
+            this.textContent = "Read";
+        }
+    })
+
+    removeButton.addEventListener("click", function() {
+        cardDiv.remove();
+        myLibrary.pop();
+    })
+
+    buttonRow.appendChild(readButton);
+    buttonRow.appendChild(removeButton);
+    cardDiv.appendChild(titleH);
+    cardDiv.appendChild(authorH);
+    cardDiv.appendChild(pagesH);
+    cardDiv.appendChild(buttonRow);
+    bookGrid.appendChild(cardDiv);
 }
